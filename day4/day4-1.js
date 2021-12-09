d630fa081aa0ecdfc97e0b47b0f0b4b6b4a8a239
Jamil Lawrence <jamil.lawrence@gmail.com>
2021-12-08 21:20:30 -0600

    Turns out diagonal weren't needed
    Maybe we'll need them later? I'll leave them in for now

--- day4/day4-1.js
+++ day4/day4-1.js
@@ -5,7 +5,7 @@ const file = fs.readFileSync(join(__dirname, './sample-input.txt'), 'utf8');
 
 const allData = file.split('\n\n');
 
-const drawNumbers = allData[0];
+const drawNumbers = allData[0].split(',');
 
 const formatBoard = (board) => board
 .split('\n')
@@ -68,10 +68,9 @@ const checkCols = (board) => {
 
 // Top left to bottom right diagonal
 const checkMainDiagonal = (board) => {
- const numRows = board.length;
+  const numRows = board.length;
   let result = true;
   for (let i = 0; i < numRows; i++) {
-    console.log('i: ', i,board[i][i].marked);
     result = result && board[i][i].marked;
   }
   return result;
@@ -82,12 +81,87 @@ const checkAltDiagonal = (board) => {
   const numRows = board.length;
   let result = true;
   for (let i = 0; i < numRows; i++) {
-    console.log('i: ', i, 'numRows - i - 1', numRows - i -1, 'marked', board[numRows - i - 1][i].marked);
     result = result && board[i][numRows - i - 1].marked;
   }
   return result;
-}
+};
 
+const checkForWin = (board) => {
+  return checkRows(board) || checkCols(board);
+  //|| checkMainDiagonal(board) || checkAltDiagonal(board);
+};
+
+const getUnmarkedSum = (board) => {
+  let sum = 0;
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board.length; j++) {
+      if (board[i][j].marked === false) {
+        sum += Number(board[i][j].val);
+      }
+    }
+  }
+  return sum;
+};
+const calcFinalValue = (board, valAsString) => {
+  return getUnmarkedSum(board) * Number(valAsString);
+};
+
+
+// console.log('!#$@$', calcFinalValue([
+//   [
+//     { val: '14', marked: true },
+//     { val: '21', marked: true },
+//     { val: '17', marked: true },
+//     { val: '24', marked: true },
+//     { val: '4', marked: true }
+//   ],
+//   [
+//     { val: '10', marked: false },
+//     { val: '16', marked: false },
+//     { val: '15', marked: false },
+//     { val: '9', marked: true },
+//     { val: '19', marked: false }
+//   ],
+//   [
+//     { val: '18', marked: false },
+//     { val: '8', marked: false },
+//     { val: '23', marked: true },
+//     { val: '26', marked: false },
+//     { val: '20', marked: false }
+//   ],
+//   [
+//     { val: '22', marked: false },
+//     { val: '11', marked: true },
+//     { val: '13', marked: false },
+//     { val: '6', marked: false },
+//     { val: '5', marked: true }
+//   ],
+//   [
+//     { val: '2', marked: true },
+//     { val: '0', marked: true },
+//     { val: '12', marked: false },
+//     { val: '3', marked: false },
+//     { val: '7', marked: true }
+//   ]
+// ], '24'));
+
+/* Main function */
+const playGame = (nums, boards) => {
+  for (let i = 0; i < nums.length; i++) {
+    // Go through the array of boards and mark the val
+    // console.log('nums[i]', nums[i]);
+    for (let j = 0; j < boards.length; j++) {
+      markBoard(boards[j], nums[i]);
+      if (checkForWin(boards[j])) {
+        // console.log('winner', boards[j]);
+        // console.log('JJJJJJJJJJ', calcFinalValue(boards[j], nums[i]));
+        return calcFinalValue(boards[j], nums[i]);
+      }
+    }
+  }
+};
+
+console.log('main', playGame(drawNumbers, transformedBoards));
 /* Quick tests */
 // console.log(boards[1]);
 // console.log(transformedBoards[1]);
@@ -102,43 +176,44 @@ const checkAltDiagonal = (board) => {
 //   { val: '25', marked: true },
 //   { val: '23', marked: true }
 // ]));
-console.log('****', checkAltDiagonal([
-  [
-    { val: '3', marked: false },
-    { val: '15', marked: false },
-    { val: '0', marked: false },
-    { val: '2', marked: false },
-    { val: '22', marked: true }
-  ],
-  [
-    { val: '9', marked: false },
-    { val: '18', marked: false },
-    { val: '13', marked: false },
-    { val: '17', marked: true },
-    { val: '5', marked: false }
-  ],
-  [
-    { val: '19', marked: false },
-    { val: '8', marked: false },
-    { val: '7', marked: true },
-    { val: '25', marked: false },
-    { val: '23', marked: false }
-  ],
-  [
-    { val: '20', marked: false },
-    { val: '11', marked: true },
-    { val: '10', marked: false },
-    { val: '24', marked: false },
-    { val: '4', marked: false }
-  ],
-  [
-    { val: '14', marked: true },
-    { val: '21', marked: false },
-    { val: '16', marked: false },
-    { val: '12', marked: false },
-    { val: '6', marked: false }
-  ]
-]));
+
+// console.log('****', checkAltDiagonal([
+//   [
+//     { val: '3', marked: false },
+//     { val: '15', marked: false },
+//     { val: '0', marked: false },
+//     { val: '2', marked: false },
+//     { val: '22', marked: true }
+//   ],
+//   [
+//     { val: '9', marked: false },
+//     { val: '18', marked: false },
+//     { val: '13', marked: false },
+//     { val: '17', marked: true },
+//     { val: '5', marked: false }
+//   ],
+//   [
+//     { val: '19', marked: false },
+//     { val: '8', marked: false },
+//     { val: '7', marked: true },
+//     { val: '25', marked: false },
+//     { val: '23', marked: false }
+//   ],
+//   [
+//     { val: '20', marked: false },
+//     { val: '11', marked: true },
+//     { val: '10', marked: false },
+//     { val: '24', marked: false },
+//     { val: '4', marked: false }
+//   ],
+//   [
+//     { val: '14', marked: false },
+//     { val: '21', marked: false },
+//     { val: '16', marked: false },
+//     { val: '12', marked: false },
+//     { val: '6', marked: false }
+//   ]
+// ]));
 
 
 
