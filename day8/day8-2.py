2351eea204f17902135b5717ca531790929bb0db
Jamil Lawrence <jamil.lawrence@gmail.com>
2022-01-10 21:25:37 -0600

    Making progress in Python

--- day8/day8-2.py
+++ day8/day8-2.py
@@ -2,6 +2,22 @@ from pprint import pprint as pp
 
 data = open('sample-input.txt').read().strip().split('\n')
 
+def sortString(input):
+  return ''.join(sorted(input))
+
+pp(sortString('Jamil'))
+
+transformed = list(map(lambda x:
+  list(map(lambda y: sortString(y), x.split(' '))), data))
+
+transformed2 = list(map(lambda x: ' '.join(x), transformed))
+
+inputs = [line.split('|')[0].strip() for line in transformed2]
+outputs = [line.split('|')[1].strip() for line in transformed2]
+
+pp(inputs)
+pp(outputs)
+
 original = [1, 2, 3, 4]
 
 doubled = list(map(lambda x: x + x, original))
