d06e34ea1f5e8cc0cf8aa5d564e1c99bf7cad861
Jamil Lawrence <jamil.lawrence@gmail.com>
2022-01-14 18:22:42 -0600

    Day Part 1 complete

--- day9/day9-1.py
+++ day9/day9-1.py
@@ -1,16 +1,44 @@
 from pprint import pprint as pp
 
-data = open('sample-input.txt').read().strip().split('\n')
+with open('input.txt') as f:
+    data = f.read().strip().split('\n')
 
-transformed = [list(row) for row in data]
-transformed = [[int(value) for value in row] for row in transformed]
+transformed = [[int(value) for value in row] for row in data]
 
+def solve(input):
+    left = False
+    up = False
+    right = False
+    down = False
 
-print(data)
-print(transformed[1][4])
-print(transformed)
+    sum = 0
+    num_rows = len(input)
+    num_cols = len(input[0])
 
-# def solve(input):
+    for row in range(num_rows):
+        for col in range(num_cols):
+            current = input[row][col]
+            if row == 0:
+                up = True
+            else:
+                up = input[row - 1 ][col] > current
+            if row == num_rows - 1:
+                down = True
+            else:
+                down = input[row + 1][col]  > current
+            if col == 0:
+                left = True
+            else:
+                left = input[row][col - 1] > current
+            if col == num_cols - 1:
+                right = True
+            else:
+                right = input[row][col + 1] > current
 
+            if left and right and up and down:
+                # print('row ', row, 'col ', col, 'val ', current)
+                sum += current + 1
 
-# print('answer', solve())
+    return sum
+
+print('answer', solve(transformed))
