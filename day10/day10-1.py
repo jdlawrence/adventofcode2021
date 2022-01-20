3a8224597744ffc11341522cc4daacbe4453bf65
Jamil Lawrence <jamil.lawrence@gmail.com>
2022-01-20 16:39:31 -0600

    Day 10 Part 1 complete

--- day10/day10-1.py
+++ day10/day10-1.py
@@ -1,7 +1,60 @@
 with open('input.txt') as f:
     data = f.read().strip().split('\n')
 
-print(data)
+data = [list(line) for line in data]
 
+# May be used in part 2?
+def handle(char):
+    if char == '(':
+        return ')'
+    elif char == '{':
+        return '}'
+    elif char == '<':
+        return '>'
+    elif char == '[':
+        return ']'
 
-print('answer', solve(transformed))
+def calc_score(char):
+    if char == ')':
+        return 3
+    elif char == ']':
+        return 57
+    elif char == '}':
+        return 1197
+    elif char == '>':
+        return 25137
+
+def solve(input):
+    score = 0;
+    for line in data:
+        stack = []
+        for char in line:
+            if char == ')':
+                if stack[-1] == '(':
+                    stack.pop()
+                else:
+                    score += calc_score(char)
+                    break
+            elif char == '}':
+                if stack[-1] == '{':
+                    stack.pop()
+                else:
+                    score += calc_score(char)
+                    break
+            elif char == '>':
+                if stack[-1] == '<':
+                    stack.pop()
+                else:
+                    score += calc_score(char)
+                    break
+            elif char == ']':
+                if stack[-1] == '[':
+                    stack.pop()
+                else:
+                    score += calc_score(char)
+                    break
+            else:
+                stack.append(char)
+    return score
+
+print('answer', solve(data))
