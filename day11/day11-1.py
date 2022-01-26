8635eb7199477d7ca581b00860b2d71aa00d0971
Jamil Lawrence <jamil.lawrence@gmail.com>
2022-01-25 18:27:52 -0600

    Stuck for now. Debugging.

--- day11/day11-1.py
+++ day11/day11-1.py
@@ -5,7 +5,7 @@ with open('sample-input.txt') as f:
 
 transformed = [[{'val': int(value), 'flashed': False } for value in row] for row in data]
 
-rounds = 10
+ROUNDS = 3
 
 def increment_all(input):
     rows = len(input)
@@ -15,24 +15,71 @@ def increment_all(input):
         for c in range(cols):
             input[r][c]['val'] += 1
 
+def flash_neighbors(input, row, col):
+    rows = len(input)
+    cols = len(input[0])
+
+    print('flashing', row, col)
+    # print_grid(input)
+
+    # Mark the octopus as flashed
+    input[row][col]['flashed'] = True
+
+    # Increment all neighbors
+    for (dr, dc) in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+        if row + dr > -1 and row + dr < rows and col + dc > -1 and col + dc < cols:
+            # print('row + dr', row + dr, 'col + dc', col + dc)
+            input[row + dr][col + dc]['val'] += 1
+
+    # print_grid(input)
+    # pp(input)
+
+    # Go through the grid again and call flash_neighbors on all appropriate octopi
+    for r in range(rows):
+        for c in range(cols):
+            if input[r][c]['val'] > 9 and input[r][c]['flashed'] == False:
+                # input[r][c]['flashed'] = True
+                # flash_neighbors(input, r, c)
+                print('jamil')
+
 def run_step(input):
     rows = len(input)
     cols = len(input[0])
 
+    # First, increase the level of each octopus
     for r in range(rows):
         for c in range(cols):
             input[r][c]['val'] += 1
-            if input[r][c]['val'] == 10:
-                input[r][c]['flashed'] = True
+
+    # Then, go through and flash neighbors
+    for r in range(rows):
+        for c in range(cols):
+            if input[r][c]['val'] > 9:
+                flash_neighbors(input, r, c)
+
+    # Finally, go through and set all flashed octopi to a value of zero
+    for r in range(rows):
+        for c in range(cols):
+            input[r][c]['flashed'] = False
+            if input[r][c]['val'] > 9:
                 input[r][c]['val'] = 0
 
+def print_grid(input):
+    rows = len(input)
+    cols = len(input[0])
+
+    # First, increase the level of each octopus
+    for r in range(rows):
+        row = []
+        for c in range(cols):
+            row.append(input[r][c]['val'])
+        print(row)
+
+    print('#################')
 
 def solve(input):
-    pp(input)
-    run_step(input)
-    pp('\n')
-    pp(input)
-    # for round in range(rounds):
-    #     print(round)
+    for round in range(ROUNDS):
+        run_step(input)
+        print_grid(input)
 
 print(solve(transformed))
