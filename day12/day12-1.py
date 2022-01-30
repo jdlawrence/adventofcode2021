7c30d2b406fac65113f7b8a38bfb96b7320cbd84
Jamil Lawrence <jamil.lawrence@gmail.com>
2022-01-30 17:57:55 -0600

    Part 1 cleanup

--- day12/day12-1.py
+++ day12/day12-1.py
@@ -5,24 +5,6 @@ class Graph(object):
     def __init__(self, graph_dict=None):
         self._graph_dict = {}
 
-    def classify(self, vertex):
-        if vertex == 'start':
-            return 'start'
-        elif vertex == 'end':
-            return 'end'
-        elif vertex.isupper():
-            return 'big'
-        else:
-            return 'small'
-
-    def add_vertex(self, vertex):
-        if vertex not in self._graph_dict:
-            self._graph_dict[vertex] = {
-                'name': vertex,
-                'type': self.classify(vertex),
-                'vertices': []
-            }
-
     def add_edge(self, edge):
         """ assumes that edge is of type set, tuple or list;
             between two vertices can be multiple edges!
@@ -34,26 +16,6 @@ class Graph(object):
                 self._graph_dict[x].append(y)
             else:
                 self._graph_dict[x] = [y]
-
-    def find_path(self, start_vertex, end_vertex, path=None):
-        """ find a path from start_vertex to end_vertex
-            in graph """
-        if path == None:
-            path = []
-        graph = self._graph_dict
-        path = path + [start_vertex]
-        if start_vertex == end_vertex:
-            return path
-        if start_vertex not in graph:
-            return None
-        for vertex in graph[start_vertex]:
-            if vertex not in path:
-                extended_path = self.find_path(vertex,
-                                               end_vertex,
-                                               path)
-                if extended_path:
-                    return extended_path
-        return None
 
     def find_all_paths(self, start_vertex, end_vertex, path=[]):
         """ find all paths from start_vertex to
